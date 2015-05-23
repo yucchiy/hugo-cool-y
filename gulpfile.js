c69317b064062537f7162dc20e7a57bc2f4d5a78
Yuichiro Mukai <y.iky917@gmail.com>
2015-05-24 04:41:45 +0900

    finish designing

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,18 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var cssnext = require('gulp-cssnext');
 
+gulp.task('stylesheets', function() {
+  gulp.src('assets/stylesheets/application.css')
+  .pipe(postcss([
+    require('postcss-import')
+  ]))
+  .pipe(cssnext())
+  .pipe(gulp.dest('static/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/stylesheets/**/*.css', ['stylesheets']);
+});
+
+gulp.task('default', ['watch']);
